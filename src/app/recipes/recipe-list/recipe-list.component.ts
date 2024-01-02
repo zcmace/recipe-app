816859b398284ce352bd5b8c480d8fc282ce6f52
Zach Mace <zmace@nettracer.aero>
2024-01-02 01:06:08 -0500

    Added recipe item, list, details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[] = [
+    new Recipe('TestRecipe', 'Test description for recipe', 'https://picsum.photos/200/300'),
+    new Recipe('AnotherRecipe', 'Woo Recipes', 'https://picsum.photos/200/300')
+  ];
+  
+  constructor() {
 
+  }
+
+  ngOnInit() {
+
+  }
 }
